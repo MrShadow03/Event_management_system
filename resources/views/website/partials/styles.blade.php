0eb41513b736428ae280415463e8ded4b0c0847b
Galib <gj.emon35@gamil.com>
2023-10-09 15:01:44 +0600

    fixed

--- resources/views/website/partials/styles.blade.php
+++ resources/views/website/partials/styles.blade.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" href="{{ asset('./assets/website/assets/font/fontawame/css/all.css') }}">
 <link rel="stylesheet" href="{{ asset('./assets/website/assets/css/owl.carousel.min.css') }}">
 <link rel="stylesheet" href="{{ asset('./assets/website/assets/css/swiper-bundle.min.css') }}">
-<link rel="stylesheet" href="{{ asset('./assets/website/assets/css/style.min.css') }}">
+<link rel="stylesheet" href="{{ asset('./assets/website/assets/css/main.min.css') }}">
